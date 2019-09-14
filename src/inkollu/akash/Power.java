274deb9c879a2cf1ccdfed6ca31a71fa57a7d1c2
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2019-09-14 19:15:41 +0530

    power name modified

--- src/inkollu/akash/Power.java
+++ src/inkollu/akash/Power.java
@@ -12,14 +12,15 @@ public class Power {
         return pow(m , n - 1) * m;
     }
 
-    private static int pow1(int m, int n) {
+    // power with reduced activation records in stack
+    private static int powReducedMul(int m, int n) {
         if (n == 0) return 1;
-        if (n % 2 == 0) return pow1(m * m, n / 2);
-        return pow1(m * m, (n - 1) / 2) * m;
+        if (n % 2 == 0) return powReducedMul(m * m, n / 2);
+        return powReducedMul(m * m, (n - 1) / 2) * m;
     }
 
     public static void main(String[] args) {
         System.out.println("Power: " + pow(2, 5));
-        System.out.println("Power: " + pow(2, 10));
+        System.out.println("Power: " + powReducedMul(2, 10));
     }
 }
