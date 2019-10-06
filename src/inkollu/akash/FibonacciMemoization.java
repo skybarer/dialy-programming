80f681d1f7ec02fc646fef5e486421cd1de9c694
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2019-10-06 20:00:32 +0530

    fibonacci memoization

--- src/inkollu/akash/FibonacciMemoization.java
+++ src/inkollu/akash/FibonacciMemoization.java
@@ -10,13 +10,9 @@ public class FibonacciMemoization {
     private static int memArray[] = new int[26];
 
     private static int fib(int n) {
-        if (n <= 1) {
-            return n;
-        } else if (memArray[n] != 0) {
-            return memArray[n];
-        } else {
-            memArray[n] = fib(n - 2) + fib(n - 1);
-        }
+        if (n <= 1) return n;
+        else if (memArray[n] != 0) return memArray[n];
+        else memArray[n] = fib(n - 2) + fib(n - 1);
         return memArray[n];
     }
 
